670de0c975bd49d2e6487ec12d06d94c746c17fe
sg0222232 <Mahesh.Tummala@Sabre.com>
2021-07-03 09:35:46 +0530

    testing

--- src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -11,7 +11,7 @@ public class CurrencyExchangeServiceApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Welcome to Docker!!!!!";
+		return "Welcome to Docker!!!!!!!!!";
 	}
 
 	public static void main(String[] args) {
